94e2a1dbca1ed9bfa55a4dcd831b50df461a1321
Ryan (Chiungting) Lin <ryanctlin@hotmail.com>
2021-01-24 11:32:10 +0000

    AutoML API working

--- ryan-dev/tweet-classification/yes_no_stock.py
+++ ryan-dev/tweet-classification/yes_no_stock.py
@@ -1,10 +1,31 @@
+import sys
+from google.api_core.client_options import ClientOptions
 from google.oauth2 import service_account
-from google.cloud import automl
+from google.cloud import automl_v1
 from google.cloud import language_v1
 
 # https://googleapis.dev/python/google-api-core/latest/auth.html
-#client = datastore.Client()
-credentials = service_account.Credentials.from_service_account_file('rapid-hall-302622-bc1821ff2bd1.json')
-client = automl.AutoMlClient(credentials=credentials)
+# client = datastore.Client()
+credentials = service_account.Credentials.from_service_account_file('rapid-hall-302622-7d92a5d1344e.json')
+#client = automl.AutoMlClient(credentials=credentials)
 
 
+# Function for reading txt file for prediction
+def inline_text_payload(file_path):
+    with open(file_path, 'rb') as ff:
+        content = ff.read()
+    return {'text_snippet': {'content': content, 'mime_type': 'text/plain'}}
+
+
+def get_prediction(file_path, model_name):
+    options = ClientOptions(api_endpoint='automl.googleapis.com')
+    prediction_client = automl_v1.PredictionServiceClient(client_options=options, credentials=credentials)
+
+    text_snip = inline_text_payload(file_path)
+    payload = automl_v1.ExamplePayload(text_snip)
+    print(payload)
+    request = prediction_client.predict(name=model_name, payload=payload)
+    return request  # waits until request is returned
+
+
+print(get_prediction('./stock_tweets/y_5.txt', 'projects/313817029040/locations/us-central1/models/TCN8645127876691099648'))
