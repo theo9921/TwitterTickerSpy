194d4de1c0f79422a9237bbf2f381717cb9e78af
Ryan (Chiungting) Lin <ryanctlin@hotmail.com>
2021-01-24 14:13:17 +0000

    Fix merge conflict

--- development-files/tweet-classification/yes_no_stock.py
+++ development-files/tweet-classification/yes_no_stock.py
@@ -6,7 +6,6 @@ from google.cloud import language_v1
 
 # https://googleapis.dev/python/google-api-core/latest/auth.html
 # client = datastore.Client()
-credentials = service_account.Credentials.from_service_account_file('rapid-hall-302622-7d92a5d1344e.json')
 #client = automl.AutoMlClient(credentials=credentials)
 
 
@@ -19,10 +18,11 @@ def inline_text_payload(file_path):
 
 def get_prediction(file_path, model_name):
     options = ClientOptions(api_endpoint='automl.googleapis.com')
+    credentials = service_account.Credentials.from_service_account_file('rapid-hall-302622-7d92a5d1344e.json')
     prediction_client = automl_v1.PredictionServiceClient(client_options=options, credentials=credentials)
 
-    text_snip = inline_text_payload(file_path)
-    #text_snip = {'text_snippet': {'content': "this is some test string", 'mime_type': 'text/plain'}}
+    #text_snip = inline_text_payload(file_path)
+    text_snip = {'text_snippet': {'content': "this is some test string", 'mime_type': 'text/plain'}}
     payload = automl_v1.ExamplePayload(text_snip)
     print(payload)
     request = prediction_client.predict(name=model_name, payload=payload)
--- tweet-processing/gcloud_api.py
+++ tweet-processing/gcloud_api.py
@@ -5,3 +5,17 @@ from google.oauth2 import service_account
 from google.cloud import automl_v1
 from google.cloud import language_v1
 
+
+def stock_tweet_classifier(tweet_string, model_name):
+    """Passes tweet into trained AutoML model, outputs classification on whether it is stock-related"""
+
+    options = ClientOptions(api_endpoint='automl.googleapis.com')
+    credentials = service_account.Credentials.from_service_account_file('AutoMLAuth.json')
+    prediction_client = automl_v1.PredictionServiceClient(client_options=options, credentials=credentials)
+
+    text_snip = {'text_snippet': {'content': tweet_string, 'mime_type': 'text/plain'}}
+    payload = automl_v1.ExamplePayload(text_snip)
+    # print(payload)
+    request = prediction_client.predict(name=model_name, payload=payload)
+
+    return request
